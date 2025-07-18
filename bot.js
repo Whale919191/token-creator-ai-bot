08a64b1fa9bd7e6a02a0ce12ccde62e0f72c0784
Whale919191 <whalezeus@proton.me>
2025-07-18 16:54:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 const app = express();
+app.use(express.json());
 
 const TOKEN = process.env.BOT_TOKEN;
 const BASE_URL = process.env.BASE_URL;
